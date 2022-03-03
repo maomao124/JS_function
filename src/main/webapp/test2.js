2199b05bf7076d89c297fdcc58692bbd82fea8c5
毛 <1296193245@qq.com>
2022-03-03 13:16:26 +0800

    JS_function

---
+++ src/main/webapp/test2.js
@@ -0,0 +1,20 @@
+/**
+ * Project name(项目名称)：JS_function
+ * File name(文件名): test2
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/3
+ * Time(创建时间)： 13:10
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+function sayHello(name = "world")
+{
+    document.write("Hello " + name);
+}
+
+sayHello();
+document.writeln("<br>");
+sayHello("world1");
---
+++ src/main/webapp/test3.js
@@ -0,0 +1,21 @@
+/**
+ * Project name(项目名称)：JS_function
+ * File name(文件名): test3
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/3
+ * Time(创建时间)： 13:12
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+function getSum(num1, num2)
+{
+    return num1 + num2;
+}
+
+var sum1 = getSum(4, 11);
+var sum2 = getSum(-9, 13);
+console.log(sum1);
+console.log(sum2);
